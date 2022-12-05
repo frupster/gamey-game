2331909cb9cf01df2af378844e899a6bb42421b5
frupster <117213809+frupster@users.noreply.github.com>
2022-12-05 15:50:44 -0500

    more trees + better player controller imported
    experimented with new player controller from asset store
    
    more trees and finished dirt

--- Stem POE project/Assets/Scripts/DeathBox.cs
+++ Stem POE project/Assets/Scripts/DeathBox.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class DeathBox : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject deathBox;
     public Vector3 SpawnPoint;
+    public bool respawn = false;
+    SC_FPSController cc;
+    //public GameObject SpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<SC_FPSController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(respawn == true)
+        {
+            cc.enabled = true;
+           respawn = false;
+        }
     }
     void OnTriggerEnter (Collider col)
     {
-        if(col.tag == "Player")
+        if(col.CompareTag("DeathBox"))
         {
-            player.transform.position = SpawnPoint;
+            Debug.Log("touched deathbox");
+            //GetComponent<SC_FPSController>().enabled = false;
             
-            
+            cc.enabled = false;
+            respawn = true;
+            transform.position = SpawnPoint;
+           
+
         }
     }
 }
